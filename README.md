738631664565e18f61f5ee607c9c15af68aebe69
Mauricio León <87592892+mleonc1977@users.noreply.github.com>
2022-07-26 10:12:01 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+# Kubernetes-Dev
+Script for Devs, it installs Docker, Docker-Compose, Kubectl and kind
+
+Works on Ubuntu 20.04+.
+Must be run as root.
+Download the install-all.sh file, chmod +x it and run.
+
+Enjoy!
+
+If you belieave in my work, 
+
+<a href="https://www.buymeacoffee.com/mleonc" target="_blank"><img src="https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png" alt="Buy Me A Coffee" style="height: 41px !important;width: 174px !important;box-shadow: 0px 3px 2px 0px rgba(190, 190, 190, 0.5) !important;-webkit-box-shadow: 0px 3px 2px 0px rgba(190, 190, 190, 0.5) !important;" ></a>
+
+[![Donate](https://img.shields.io/badge/Donate-PayPal-green.svg)](https://www.paypal.com/donate/?hosted_button_id=6MSLWSDZ8LPP4)
+
---
+++ install_all.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#### Docker install ####
+sudo apt-get -y remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get -y install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+
+####Docker-compose install ####
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+####kubectl install ####
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+
+#### Go and kind install ####
+#sudo rm -rf /usr/local/go 
+#sudo tar -C /usr/local -xzf go1.18.2.linux-amd64.tar.gz
+
+#go install sigs.k8s.io/kind@v0.14.0
+
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.14.0/kind-linux-amd64
+chmod +x ./kind
+mv ./kind /usr/local/bin/kind
